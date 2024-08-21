dd83216c5c49b72714976dcc03fd13954a601e42
Carlos Lostao <hastro1990@gmail.com>
2024-08-21 16:47:46 +0200

    fix: reclaim protocol qr

--- frontend/src/components/ReclaimQRCode.tsx
+++ frontend/src/components/ReclaimQRCode.tsx
@@ -31,12 +31,15 @@ export const ReclaimQRCode = ({
         <Modal isOpen={true} className="flex flex-col" onRequestClose={onDismiss} preventScroll={true}>
             <div className="flex flex-col w-1/2 bg-slate-50 h-content absolute top-0 bottom-0 left-0 right-0 m-auto p-4 gap-2">
                 <div className="text-center flex flex-col items-center gap-2">
-                    <span className="flex flex-row text-sm text-gray-500 items-center gap-4">1. Install Reclaim Protocol app
+                    <span>
+                        1. Install Reclaim Protocol app
+                    </span>
+                    <span className="flex flex-row text-sm text-gray-500 items-center gap-4">
                         <a href='https://play.google.com/store/apps/details?id=com.reclaim.protocol&hl=en&pli=1' target='_blank'>
-                            <img className='h-[32px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb5LOPUgzjbz_m4aVulC-GU5zu-30HBdYnAg&s' />
+                            <img className='h-8 aspect-square rounded' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb5LOPUgzjbz_m4aVulC-GU5zu-30HBdYnAg&s' />
                         </a>
                         <a href='https://apps.apple.com/in/app/reclaim-protocol/id6475267895' target='_blank'>
-                            <img className='h-[32px]' src='https://www.apple.com/v/app-store/b/images/overview/icon_appstore__ev0z770zyxoy_large_2x.png' />
+                            <img className='h-8 aspect-square rounded' src='https://www.apple.com/v/app-store/b/images/overview/icon_appstore__ev0z770zyxoy_large_2x.png' />
                         </a>
                     </span>
                     <p className="text-sm text-gray-500">2. Scan the QR code to create your claim</p>
