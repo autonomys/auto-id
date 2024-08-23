efef704f590fb8a859333a27e7fb26cfa58683b1
Carlos Lostao <hastro1990@gmail.com>
2024-08-23 18:42:18 +0200

    Revert "update: do not handle imposible case"
    This reverts commit 4f098071d484ab743ced675ad06da1c242dd1c57.

--- app/src/app/auto-id/[id]/page.tsx
+++ app/src/app/auto-id/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from 'next/navigation';
 import { AutoIdDetails } from '../../../components/AutoIdDetails/AutoIdDetails';
 import { DiscordAction, discordAuthUrl } from '../../../services/auth/discord';
 
 
 export default function AutoId({ params }: { params: { id: string } }) {
     const { id } = params;
+    if (!id) {
+        notFound()
+    }
 
     const linkToDiscordUrl = discordAuthUrl({
         action: DiscordAction.AutoIdVerification,
