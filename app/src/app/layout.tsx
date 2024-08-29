dd695794ba575b066d856d2b13c77cffee13a3a0
Carlos Lostao <hastro1990@gmail.com>
2024-08-29 11:47:34 +0200

    update: make header less ugly

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -20,10 +20,15 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="fixed w-screen h-screen z-back bg-gradient-to-br from-blue-100 via-white to-green-100" />
         <Toaster position="bottom-center" />
-        <div className="h-screen w-screen flex-col">
-          <header className="flex flex-row gap-2 items-center h-[7.5%] p-2 font-semibold text-[#929EEA] ml-2">
-            <img src="/autonomys.png" alt="Autonomys" className="h-full" />
-            <h1 className="text-4xl">Autonomys</h1>
+        <div className="h-screen w-screen flex flex-col items-center">
+          <header className="flex flex-col gap-1 items-center p-2 font-semibold text-[#929EEA] ml-2 justify-center md:w-[60%] w-[80%]">
+            <h3 className="opacity-70">
+              Auto-ID powered by
+            </h3>
+            <div className="flex flex-row h-8 items-center gap-2">
+              <img src="/autonomys.png" alt="Autonomys" className="h-full" />
+              <h1 className="text-4xl">Autonomys</h1>
+            </div>
           </header>
           <main
             className={`h-[92.5%] w-full flex justify-center items-center relative`}
